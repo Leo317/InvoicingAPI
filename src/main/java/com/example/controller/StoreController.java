60efc5db7062305f1c0217fc62a98afeb34ef967
Kevin CY Chen <Kevin_CY_Chen@wistron.com>
2018-03-18 20:25:29 +0800

    InvoicingAPI - modify apis of store.

--- src/main/java/com/example/controller/StoreController.java
+++ src/main/java/com/example/controller/StoreController.java
@@ -1,8 +1,11 @@
 package com.example.controller;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.model.Products;
@@ -12,16 +15,16 @@ import com.example.page.Status;
 
 @RestController
 public class StoreController {
-  @RequestMapping(value = "/puchase", method = RequestMethod.POST, produces = {"application/json"})
-    public Response orderProudcts(@RequestBody Products[] products) {
+  @RequestMapping(value = "/purchase", method = RequestMethod.POST, produces = {"application/json"})
+    public Response purchase(@RequestBody Products[] products) {
 
 			
 	  return new AjaxResponse(Status.SUCCESS, "", null);
   }
   
   @RequestMapping(value = "/list", method = RequestMethod.GET, produces = {"application/json"})
-  public void purchase() {
-
+  public List<Products> list(@RequestParam(value="keyword", defaultValue="") String keyword) {
+	return null;
   }
   
 
