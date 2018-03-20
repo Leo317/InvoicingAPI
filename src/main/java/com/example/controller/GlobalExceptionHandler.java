2cec13e115373a1cdf7a860be73cdefb3beeb1c4
Kevin CY Chen <Kevin_CY_Chen@wistron.com>
2018-03-20 17:13:43 +0800

    InvoicingAPI - error handling.

---
+++ src/main/java/com/example/controller/GlobalExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.example.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.example.page.AjaxResponse;
+import com.example.page.Response;
+import com.example.page.Status;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+  private Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+  @ResponseBody
+  @ExceptionHandler(Exception.class)
+  public Response handleException(Exception e) {
+	String msg = e.getMessage();
+	if(StringUtils.isEmpty(msg)) {
+	  msg = "Server exception occurred.";
+	}
+	logger.error(e.getMessage());
+	return new AjaxResponse(Status.ERROR, msg, null);	  
+  }
+}
