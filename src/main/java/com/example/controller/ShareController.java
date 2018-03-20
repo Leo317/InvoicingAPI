6058e5eae1687299f33b47a4f546e6def1c85016
Leo <Leo_Kuo@wistron.com>
2018-03-20 12:11:20 +0800

    fix share func.

--- src/main/java/com/example/controller/ShareController.java
+++ src/main/java/com/example/controller/ShareController.java
@@ -1,7 +1,5 @@
 package com.example.controller;
 
-import java.util.List;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,7 +13,6 @@ import com.example.page.Response;
 import com.example.page.Status;
 import com.example.service.CommonTools;
 import com.example.service.IShareService;
-import com.example.view.OrdersDTO;
 
 @RestController
 @RequestMapping("/share")
@@ -24,19 +21,17 @@ public class ShareController {
 	IShareService shareServ;
 	
 	private static final Logger logger = LoggerFactory.getLogger(ShareController.class);
-
-	@RequestMapping(value = "/getOrdersList", method = RequestMethod.GET, produces = {"application/json"})
-	public List<OrdersDTO> findAll() {
-		return shareServ.findAll();
-	}
 	
 	@RequestMapping(value = "/getOrderList", method = RequestMethod.GET, produces = {"application/json"})
-	public Response findOne(@RequestParam("id") String id) {
+	public Response findOne(@RequestParam(value = "id", required = true, defaultValue = "0") String id) {
 		// http://127.0.0.1:8080/share/getOrderList?id=1.aaa
 		if (id.indexOf('.') > 0 || CommonTools.isInteger(id) == false)
 			return new AjaxResponse(Status.STATUS400, "Order id : " + id + " is not an integer!!!", null);
 		else {
-			return new AjaxResponse(Status.SUCCESS, "", shareServ.findOne(Integer.parseInt(id)));
+			if (id.equals("0"))
+				return new AjaxResponse(Status.SUCCESS, "", shareServ.findAll());
+			else
+				return new AjaxResponse(Status.SUCCESS, "", shareServ.findOne(Integer.parseInt(id)));
 		}
 	}
 }
