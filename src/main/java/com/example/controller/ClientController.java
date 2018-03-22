68360374d5427983d730f1a5ebd437f232d08809
Leo <Leo_Kuo@wistron.com>
2018-03-22 13:47:06 +0800

    code fixed

--- src/main/java/com/example/controller/ClientController.java
+++ src/main/java/com/example/controller/ClientController.java
@@ -1,5 +1,7 @@
 package com.example.controller;
 
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.math.NumberUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,54 +23,67 @@ import com.example.view.ProductsDTO;
 public class ClientController {
 	@Autowired
 	IClientService clientServ;
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(ClientController.class);
 
-	@RequestMapping(value = "/getOrderableProductsList", method = RequestMethod.GET, produces = {"application/json"})
+	@RequestMapping(value = "/getOrderableProductsList", method = RequestMethod.GET, produces = { "application/json" })
 	public Response findOrderableProducts() {
 		return new AjaxResponse(Status.SUCCESS, "", clientServ.getOrderableProductsList());
 	}
-	
-	@RequestMapping(value = "/orderProducts", method = RequestMethod.POST, produces = {"application/json"})
-	public Response orderProudcts(@RequestBody ProductsDTO[] productsDTO) {
-		for (int i = 0; i < productsDTO.length; i++) {
-			if(!CommonTools.productNameLengthCheck(productsDTO[i].getProductName())) {
-		        return new AjaxResponse(Status.STATUS400, "The product name: " 
-		          + productsDTO[i].getProductName()
-		    	  + " is out of length.", null);	    	  
-		      }
-			if(!clientServ.getProductExist(productsDTO[i].getProductName())) {
-				return new AjaxResponse(Status.STATUS400, "Product name : " + productsDTO[i].getProductName()
-						+ " is not exist or not on sale!!!", null);
-			} else {
-				if (productsDTO[i].getQuantity().indexOf('.') > 0 || CommonTools.isInteger(productsDTO[i].getQuantity()) == false)
-					return new AjaxResponse(Status.STATUS400, "Proudct name : " + productsDTO[i].getProductName() + ", its quantity : " + productsDTO[i].getQuantity() + " is not an integer!!!", null);
-				else {
-					if (Integer.parseInt(productsDTO[i].getQuantity()) - clientServ.getProductQuantity(productsDTO[i].getProductName()) > 0)
-						return new AjaxResponse(Status.STATUS400, "Product name : " + productsDTO[i].getProductName()
-								+ " is not enough!!!", null);
+
+	@RequestMapping(value = "/orderProducts", method = RequestMethod.POST, produces = { "application/json" })
+	public Response orderProudcts(@RequestBody ProductsDTO[] productsDTOs) {
+		if (productsDTOs != null && productsDTOs.length > 0) {
+			for (ProductsDTO productsDTO : productsDTOs) {
+				StringBuilder str = new StringBuilder();
+				str.append("The product name : ");
+				str.append(productsDTO.getProductName());
+				if (StringUtils.isNotBlank(productsDTO.getProductName())) {
+					str.append(" is out of length.");
+
+					return new AjaxResponse(Status.STATUS400, str.toString(), null);
+				}
+
+				if (NumberUtils.isDigits(productsDTO.getQuantity())) {
+					str.append(", its quantity : ");
+					str.append(productsDTO.getQuantity());
+					str.append(" is not an integer!!!");
+
+					return new AjaxResponse(Status.STATUS400, str.toString(), null);
+				}
+
+				if (Integer.parseInt(productsDTO.getQuantity())
+						- clientServ.getProductQuantity(productsDTO.getProductName()) > 0) {
+					str.append(" is not enough!!!");
+
+					return new AjaxResponse(Status.STATUS400, str.toString(), null);
 				}
 			}
-		}
-		
-		Products[] products = new Products[productsDTO.length];
-		for (int i = 0; i < productsDTO.length; i++) {
-			Products temp = new Products();
-			temp.setProductName(productsDTO[i].getProductName());
-			temp.setQuantity(Integer.parseInt(productsDTO[i].getQuantity()));
 			
-			products[i] = temp;
-		}
+			Products[] products = new Products[productsDTOs.length];
+			int index = 0;
+			
+			for (ProductsDTO productsDTO : productsDTOs) {
+				Products temp = new Products();
+				temp.setProductName(productsDTO.getProductName());
+				temp.setQuantity(Integer.parseInt(productsDTO.getQuantity()));
+				products[index] = temp;
+				index++;
+			}
+			
+			int orderId = clientServ.getOrderId();
+			clientServ.orderProduct(orderId + 1, products);
 
-		int orderId = clientServ.getOrderId();
-		clientServ.orderProduct(orderId + 1, products);
+			return new AjaxResponse(Status.SUCCESS, "", null);
+		} else {
+			return new AjaxResponse(Status.ERROR, "paramter is null", null);
+		}
 		
-		return new AjaxResponse(Status.SUCCESS, "", null);
 	}
-	
+
 	@RequestMapping("/")
-	public String find(){
+	public String find() {
 		return "HHefwfwfewfH";
 	}
-	
+
 }
