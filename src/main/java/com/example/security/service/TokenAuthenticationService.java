d369fa4246b67b90542d56c0b955454481622a83
Kevin CY Chen <Kevin_CY_Chen@wistron.com>
2018-04-12 13:04:51 +0800

    jwt - remove unused method.

--- src/main/java/com/example/security/service/TokenAuthenticationService.java
+++ src/main/java/com/example/security/service/TokenAuthenticationService.java
@@ -25,48 +25,10 @@ public class TokenAuthenticationService {
 	public static final String ISSUER = "Wistron";
 	public static final String SUBJECT = "InvoicingAPI";
 
-//	public static void addAuthentication(HttpServletResponse res, String userId) {
-//		String jwtToken = createJwt(userId, ISSUER, SUBJECT, EXPIRATIONTIME);
-//
-//		res.addHeader(HEADER_STRING, jwtToken);
-//	}
-//	
-//	public static String createJwt(String id, String issuer, String subject, long ttlMilliseconds) {
-//
-//		SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS512;
-//		
-//		long nowMillis = System.currentTimeMillis();
-//		Date now = new Date(ttlMilliseconds);
-//		
-//		//We will sign our JWT with our API key: SECRET
-//		byte[] apiKeySecretBytes = DatatypeConverter.parseBase64Binary(SECRET);
-//		Key signingKey = new SecretKeySpec(apiKeySecretBytes, signatureAlgorithm.getJcaName());
-//		
-//		//Let's set the JWT Claims
-//		JwtBuilder builder = 
-//		  Jwts.builder().setId(id)
-//		                .setIssuedAt(now)
-//		                .setSubject(subject)
-//				        .setIssuer(issuer)
-//				        .signWith(signatureAlgorithm, signingKey);
-//		//If it has been specified, let's add the expiration
-//		if(ttlMilliseconds >= 0) {
-//		    long expMillis = nowMillis + ttlMilliseconds;
-//		    Date exp = new Date(expMillis);
-//		    builder.setExpiration(exp);
-//		}
-//		
-//		//Builds the JWT and serializes it to a compact, URL-safe string
-//		return builder.compact();
-//	}
-
 	public static Authentication getAuthentication(HttpServletRequest request) {
 		String token = request.getHeader(HEADER_STRING);
 		if (token != null) {
-			// parse the token.
-//			String user = Jwts.parser().setSigningKey(SECRET).parseClaimsJws(token).getBody()
-//					.getSubject();
-			//String user = getToken(token).getSub();
+
 			String userId = getUserIdFromToken(token);
 			if((userId != null) && (userId.isEmpty())) {
 				userId = null;
@@ -98,12 +60,9 @@ public class TokenAuthenticationService {
         String jwtToken = token;
         // Decode
         String[] split_string = jwtToken.split("\\.");
-        String base64EncodedHeader = split_string[0];
         String base64EncodedBody = split_string[1];
-        // String base64EncodedSignature = split_string[2];
         // JWT Header
         Base64 base64Url = new Base64(true);
-        //String header = new String(base64Url.decode(base64EncodedHeader));
         // JWT Body
         String body = new String(base64Url.decode(base64EncodedBody));
 		final Gson gson = new Gson();
